258039e7c9050280d75e03442f924f5ae10eb56b
Eliott <ioiottdago@gmail.com>
2025-07-25 02:10:40 +0200

    Experimenting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
