aec31607116d8c96dab2e6ce9190f218b9ef4a63
NeyZzO <eliott.dagostinoz@u-night.org>
2025-11-18 15:54:10 +0100

    Added: astrojs/sitemap integration to generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
