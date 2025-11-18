f69c521c0d78e58220718885d399c07b7180db1a
NeyZzO <eliott.dagostinoz@u-night.org>
2025-11-18 15:57:10 +0100

    Added: robots.txt for Search Engines crawlers

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://u-night.org",
   integrations: [react(), mdx(), sitemap()],
 
   vite: {
